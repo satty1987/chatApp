7d64cbb39ba9f81b8a3643e58a9e5685b1e7ed47
Satnam Singh <satnam.singh156@t-mobile.com>
2019-10-17 22:27:19 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+var users = [];
+var userList = []
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+"/index.html");
+})
+
+
+
+io.on("connection", function (socket) {
+
+
+  socket.on("chat_message", function (data) {
+    socket.broadcast.emit("chat_message", data);
+    users.push(data);
+    console.log(users);
+    socket.broadcast.emit("all", users);
+    socket.broadcast.emit("userCount", userList.length);
+  });
+
+  socket.on("disconnect", function (data) {
+    socket.broadcast.emit("user_leave", this.username);
+  });
+
+  socket.on('userConnect', function(user) {
+   
+    let obj = userList.find(obj => obj.user == user.user);
+    if (obj) {
+      socket.broadcast.emit("userCount", userList.length);
+      return;
+    } else {
+      userList.push(user);
+      socket.broadcast.emit("userCount", userList.length);
+    }
+   
+   
+    console.log(userList);
+    socket.emit("all", users);
+   
+
+  });
+});
+
+
+server.listen(3000);
